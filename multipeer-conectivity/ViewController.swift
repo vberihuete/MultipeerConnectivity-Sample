6c38c68a013c16aaaa86a97289c25c82e492d1d4
vberihuete <vincentberihuete809@gmail.com>
2018-10-15 17:36:12 -0400

    Initial Commit

---
+++ multipeer-conectivity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  multipeer-conectivity
+//
+//  Created by Vincent Berihuete on 10/15/18.
+//  Copyright © 2018 devcorerd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
